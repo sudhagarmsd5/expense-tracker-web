76bc5b34f3d4953b678d39a8291d77f418559c65
sudhagarmsd5 <sudhagarmsd@gmail.com>
2024-01-20 00:41:26 +0530

    packages installed, auth page route added

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-96">{/* <AuthForm /> */}</div>
+    </div>
+  );
+}
