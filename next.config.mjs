1b56e5fa7773a9822bcddc42f04f4f2dde348dae
sudhagarmsd5 <sudhagarmsd@gmail.com>
2024-01-21 15:07:10 +0530

    next config file updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,
+    output: 'export',
+    basePath:"",
+    images: { unoptimized: true }
+};
 
 export default nextConfig;
